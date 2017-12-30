8b5d34c04962de7f1a749499db806e49a82f8602
Shane P Drafahl <sdrafahl@iastate.edu>
2017-12-30 13:56:45 -0600

    removed comments and changed res,err,req to long names

--- myRental/app.js
+++ myRental/app.js
@@ -7,26 +7,20 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../MyRentalWeb/build')));
 
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+app.use(function(error, request, response, next) {
+    response.locals.message = error.message;
+    response.locals.error = request.app.get('env') === 'development' ? error : {};
+    response.status(error.status || 500);
+    response.render('error');
 });
 
 app.get('*', function(req, res,next) {
