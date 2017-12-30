e7da5536780866b3f2364e6a4145f5b36b67b3f9
Shane P Drafahl <sdrafahl@iastate.edu>
2017-12-30 14:52:20 -0600

    Removed duplicate readme

--- myRental/routes/userController.js
+++ myRental/routes/userController.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/create', req, res, next => {
+router.post('/create', (req, res, next) => {
   console.log("Create User");
+  
 });
 
 module.exports = router;
