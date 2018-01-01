2bcd8a3f333d9009e6e159cdd66e32c4ef9f036c
Shane P Drafahl <sdrafahl@iastate.edu>
2018-01-01 13:08:30 -0600

    Added post request so express server can register users

--- myRental/modules/database.js
+++ myRental/modules/database.js
@@ -22,7 +22,37 @@ function dataBaseModule() {
   });
 };
 
+method.registerUser = (request, result, callBack) => {
+    console.log("Registering User");
+    var password = hashPassword(request.body.password);
+    var email = request.body.email;
+    var username = request.body.username;
 
+    var checkUserSql = "SELECT * FROM USERS WHERE NAME = " + "'" + username + "'" + " OR EMAIL = " + "'" + email + "'";
+    console.log(checkUserSql);
+    this.connection.query(checkUserSql, (err, rows) => {
+        if(err) throw err;
 
+        if(rows.length == 0) {
+            var submitUserSql = "INSERT INTO USERS VALUES('" + username + "','" + password + "', NULL, '" + email  + "')";
+            console.log(submitUserSql);
+            this.connection.query(submitUserSql, (err, rows) => {
+                if(err) throws err;
+                return callBack ({
+                    success: true,
+                });
+            });
+        } else {
+            return callBack ({
+                success: false,
+            });
+        }
+    });
+}
+
+function hashPassword(password) {
+  var salt = bcrypt.genSaltSync(10);
+  return bcrypt.hashSync(password, salt);
+}
 
 module.exports = dataBaseModule;
