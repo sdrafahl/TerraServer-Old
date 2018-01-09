83bffbb041fc44f304805786dca3f7a07f9cbbb4
shane <sdrafahl@iastate.edu>
2018-01-08 21:50:54 -0600

    Removed Mock-Express untill I can research how to use it

--- myRental/scripts/setup.sh
+++ myRental/scripts/setup.sh
@@ -2,6 +2,7 @@
 echo "Setting up NPM packages..."
 sudo npm install knex@0.13 --save
 sudo npm install bookshelf --save
-sudo npm install bookshelf-check-duplicates
+sudo npm install bookshelf-check-duplicates -g
 sudo npm install mocha -g
+sudo npm install mock-express -g
 sudo npm install
--- myRental/test/test.js
+++ myRental/test/test.js
@@ -1,8 +1,21 @@
 var assert = require('assert');
+var MockExpress = require('mock-express');
+
+var app = MockExpress();
+
+/* Fake Moka Test */
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
+
+describe('userController', function() {
+  describe('post -> /create', function() {
+    it('It should at a user to the database', function() {
+
     });
   });
 });
