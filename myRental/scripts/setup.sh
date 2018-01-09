26d0011a00d9f8cbaeb9910bef88288ce47ba888
Shane P Drafahl <sdrafahl@iastate.edu>
2018-01-09 16:50:33 -0600

    Added extra module to setup script

--- myRental/scripts/setup.sh
+++ myRental/scripts/setup.sh
@@ -3,4 +3,5 @@ echo "Setting up NPM packages..."
 sudo npm install knex@0.13 --save
 sudo npm install bookshelf --save
 sudo npm install bookshelf-check-duplicates
+sudo npm install mysql
 sudo npm install
