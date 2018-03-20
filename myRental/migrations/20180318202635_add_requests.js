952b92858108b2400c33d482ebc18a51681a01a4
shane <sdrafahl@iastate.edu>
2018-03-19 20:41:58 -0500

    Modified migration to be many to many for users and requests

--- myRental/migrations/20180318202635_add_requests.js
+++ myRental/migrations/20180318202635_add_requests.js
@@ -1,13 +1,28 @@
 exports.up = function(knex, Promise) {
+    knex.schema.dropTable('USERS');
     return knex.schema.createTableIfNotExists('REQUESTS', (table) => {
         table.increments('id').primary();
         table.jsonb('JSON_REQUEST');
         table.date('CREATED');
         table.string('STATE_OF_REQUEST');
         table.string('ADDRESS');
+    }).createTable('USERS', (table) => {
+        table.increments('id').primary();
+        table.string('NAME');
+        table.string('PASSWORD');
+        table.string('EMAIL');
+        table.string('ADDRESS');
+        table.string('CITY');
+        table.integer('ZIP');
+        table.string('STATE');
+    }).createTable('USERS_REQUESTS', (table) => {
+        table.integer('USERS_id').references('USERS.id');
+        table.integer('REQUESTS_id').references('books.id');
     });
 };
 
 exports.down = function(knex, Promise) {
     return knex.schema.dropTable('REQUESTS');
+        .dropTable('USERS')
+        .dropTable('USERS_REQUESTS');
 };
